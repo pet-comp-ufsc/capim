509c5a6be0630ccca3526e1037bcd65dc3189b14
Ranieri Althoff <ranisalt@gmail.com>
2014-10-20 02:07:32 -0200

    Leitura básica de arquivo salvo

--- capim.py
+++ capim.py
@@ -3,9 +3,15 @@
 import flask
 app = flask.Flask(__name__)
 
-@app.route('/load', methods=['GET'])
-def load_data():
-	flask.abort(501)
+@app.route('/load/<ident>', methods=['GET'])
+def load_data(ident):
+	try:
+		fp = open('/'.join(['data', '.'.join([ident, 'json'])]), 'rb')
+		data = fp.read()
+		fp.close()
+	except IOError:
+		flask.abort(404)
+	return data, 200
 
 @app.route('/store', methods=['PUT'])
 def store_data():
