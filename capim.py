5e4788931a1d68980e03bb62e6cfd4274032bbfe
Ranieri Althoff <ranisalt@gmail.com>
2014-10-22 03:16:10 -0200

    Converter configuração da versão anterior para a versão nova (chamada 3.0.0-alpha)

--- capim.py
+++ capim.py
@@ -1,7 +1,73 @@
 #!/usr/bin/python3
 
+import os
 import flask
+from flask.ext import assets
+
+# CONFIGURAÇÃO
+DEBUG = True
+VERSAO = '3.0.0-alpha'
+TITLE = 'MatrUFSC'
+CAMPI = {
+		'FLO': 'Florianópolis',
+		'JOI': 'Joinville',
+		'CBS': 'Curitibanos',
+		'ARA': 'Araranguá',
+		'BLU': 'Blumenau',
+}
+SEMESTRES = ['20151', '20142',]
+PLANOS = 4
+DIAS = {
+		2: 'Segunda',
+		3: 'Terça',
+		4: 'Quarta',
+		5: 'Quinta',
+		6: 'Sexta',
+		7: 'Sábado',
+}
+HORARIOS = {
+		'0730': '07:30',
+		'0820': '08:20',
+		'0910': '09:10',
+		'1010': '10:10',
+		'1100': '11:00',
+		'1330': '13:30',
+		'1420': '14:20',
+		'1510': '15:10',
+		'1620': '16:20',
+		'1710': '17:10',
+		'1830': '18:30',
+		'1920': '19:20',
+		'2020': '20:20',
+		'2110': '21:10',
+}
+
 app = flask.Flask(__name__)
+app.config.from_object(__name__)
+env = assets.Environment(app)
+env.debug = True
+
+env.register(
+	'scripts',
+	assets.Bundle(
+		'coffee/main.coffee',
+		filters='coffeescript',
+		output='capim.js'
+	)
+)
+
+env.register(
+	'stylesheets',
+	assets.Bundle(
+		'stylus/main.styl',
+		filters='stylus',
+		output='capim.css'
+	)
+)
+
+@app.route('/', methods=['GET'])
+def index():
+	return flask.render_template('index.html')
 
 @app.route('/load/<ident>', methods=['GET'])
 def load_data(ident):
